7b4321ff29520e35b9944009796ba48708629ee3
Jerico Pingul <jerico.pingul@gmail.com>
2022-12-06 18:07:03 +0000

    Nx affected issue

---
+++ packages/my-library/README.md
@@ -0,0 +1,11 @@
+# my-library
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test my-library` to execute the unit tests via [Jest](https://jestjs.io).
+
+## Running lint
+
+Run `nx lint my-library` to execute the lint via [ESLint](https://eslint.org/).
---
+++ packages/my-library/jest.config.ts
@@ -0,0 +1,15 @@
+/* eslint-disable */
+export default {
+  displayName: 'my-library',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^.+\\.[tj]sx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/packages/my-library',
+};
